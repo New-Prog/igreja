2fa284f5c01a3c22a241a71348d33b13513591e7
Cloves Junior <cloves.junior@lab123.com.br>
2017-03-02 21:38:26 -0300

    Ajuste no login e inicio da implementação do "esqueci minha senha"

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'membros'], function () {
-    Route::get('/cadastrar', function(){ 
+    Route::get('/cadastrar', function(){
 			return view('membros_cadastrar');
 		}); // Get all members
 
     Route::post('/cadastrar/salvar', 'MembroViewController@saveMembro'); // save member
     Route::get('/consultar', 'MembroViewController@allMembros'); // Get all members
     Route::get('/alterar/{id}', 'MembroViewController@alterarMembro'); // Get all members
-    Route::post('/up/{id}', 'MembroViewController@updateMembro'); // update member     
+    Route::post('/up/{id}', 'MembroViewController@updateMembro'); // update member
 
     // Route::get('/{id}', 'MembroViewController@getMembro'); // Get member specific
 });
@@ -35,24 +35,19 @@ Route::get('/', function () {
 Auth::routes();
 Route::auth();
 
-Route::get('/getLead', 'LeadController@getLeads');
-
-Route::post('/lead', 'LeadController@store');
-
 Route::get('/listarMembros', 'MembroController@listar');
 
-#Route::get('/CadastrarMembros', 'MembrosController@cadastrar');
-
-
+//Route::get('/getLead', 'LeadController@getLeads');
+//Route::post('/lead', 'LeadController@store');
+//Route::get('/CadastrarMembros', 'MembrosController@cadastrar');
 //Route::get('/membros', 'MembroController@index');
+//Route::get('/home', 'PostController@VemNiMimView');
 
 Route::post('/membros/save', 'MembroController@store');
-
 Route::post('/membros/update', 'MembroController@update');
 
 
 
-Route::get('/home', 'PostController@VemNiMimView');
 
 Route::post('/post', 'PostController@store');
 
@@ -63,17 +58,20 @@ Route::get('/logout', function() {
 	return view('principal');
 });
 
-
-
-
 Route::get('/api/membros', 'MembroController@allMembros'); // Get all members
 Route::get('/api/membros/{id}', 'MembroController@getMembro'); // Get member specific
 Route::get('/api/membros/app/{id}', 'MembroController@getMembrosByLider'); // Get members by leader
 Route::post('/api/membros', 'MembroController@saveMembro'); // save member
-Route::post('/api/membros/up/{id}', 'MembroController@updateMembro'); // update member     
+Route::post('/api/membros/up/{id}', 'MembroController@updateMembro'); // update member
 Route::post('/api/membros/del/{id}', 'MembroController@deleteMembro'); // delete member
 
+Route::get('/dashboard', function() {
+		return view('dashboard');
+});
 
+Route::get('/forgot', function() {
+		return view('auth/passwords/email');
+});
 
 Route::get('/dashboard', function() {
 		return view('dashboard');
@@ -81,19 +79,18 @@ Route::get('/dashboard', function() {
 
 Route::get('/membros/cadastrar', 'DashboardController@teste');
 
-
 Route::get('/celulas/consultar', function() {
 		return view('celulas_consultar');
-}); 
+});
 
 Route::get('/celulas/cadastrar', function() {
 		return view('celulas_cadastrar');
-}); 
+	});
 
 Route::get('/reunioes/cadastrar', function() {
 		return view('reunioes_cadastrar');
-}); 
+});
 
 Route::get('/reunioes/cadastrar', function() {
 		return view('reunioes_cadastrar');
-}); 
+});
