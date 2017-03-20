5dcb670a8acfeb69aba535a4c3cc34d334932c29
Denis Pereira Alves <denisalves@MacBook-Air-de-Denis.local>
2017-03-20 20:45:04 -0300

    Cadastro e listagem de reunioes

--- routes/web.php
+++ routes/web.php
@@ -79,7 +79,7 @@ Route::group(['prefix' => 'membros'], function () {
 //*****************//
 Route::group(['prefix' => 'celulas'], function () {
     Route::get('/cadastrar', function() { 
-		return view('membros_cadastrar');
+		return view('celulas_cadastrar');
     }); 
 
     // Route::get('/cadastrar', 'CelulaViewController@viewCelula'); // save member
@@ -97,7 +97,7 @@ Route::group(['prefix' => 'celulas'], function () {
 
 
 //*****************//
-//* INI  - CELULAS *//
+//* INI  - REUNIOES *//
 //*****************//
 Route::group(['prefix' => 'reunioes'], function () {
     Route::get('/cadastrar', function() { 
@@ -110,16 +110,9 @@ Route::group(['prefix' => 'reunioes'], function () {
     Route::post('/alterar/{id}', 'ReuniaoViewController@alterarReuniao'); // 
 });
 
-Route::group(['prefix' => 'reunioes'], function () {
-    Route::get('/cadastrar', function() { 
-        return view('reunioes_cadastrar');
-    }); 
-
-    Route::get('/consultar', 'ReuniaoViewController@allReunioes'); // Abrir tela retornando a view
-    // Route::get('/cadastrar', 'ReuniaoViewController@saveReuniao'); //  
-    Route::post('/cadastrar/save', 'ReuniaoViewController@saveReuniao'); // 
-    Route::post('/alterar/{id}', 'ReuniaoViewController@alterarReuniao'); // 
-});
+//*****************//
+//* FIM  - REUNIOES *//
+//*****************//
 
 Route::group(['prefix' => 'posts'], function () {
     Route::get('/cadastrar', function() { 
@@ -169,30 +162,4 @@ Route::group(['prefix' => 'posts'], function () {
 
 // Route::get('/reunioes/cadastrar', function() {
 // 		return view('reunioes_cadastrar');
-// });
-
-<<<<<<< HEAD
-// <<<<<<< HEAD
-// =======
-// Route::get('/celulas/consultar', function() {
-// 		return view('celulas_consultar');
-// });
-
-// Route::get('/celulas/cadastrar', function() {
-// 		return view('celulas_cadastrar');
-// 	});
-
-// >>>>>>> 2fa284f5c01a3c22a241a71348d33b13513591e7
-Route::get('/reunioes/cadastrar', function() {
-		return view('reunioes_cadastrar');
-});
-=======
-
-// Route::get('/posts/cadastrar', function() {
-//         return view('reunioes_cadastrar');
-// });
->>>>>>> 3ef179b5869681defd93ac0f797d3b04e1e792b8
-
-// Route::get('/posts/cadastrar', function() {
-//         return view('reunioes_cadastrar');
 // });
