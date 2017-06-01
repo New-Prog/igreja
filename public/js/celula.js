3ed3495de62edeeaf032293a9fc7d66f225b15ab
Cloves Junior <cloves.junior@lab123.com.br>
2017-06-01 19:23:27 -0300

    Alterações

--- public/js/celula.js
+++ public/js/celula.js
@@ -2,10 +2,10 @@
 	'use strict';
 	$('#btn_pesquisar').on('click', pesquisar);
 	$('#filtro').on('change', carregaCampoFiltro);
-	
+
 	function carregaCampoFiltro(event) {
 		var valor = $(event.target).val();
-		
+
 		if(valor == 'lider') {
 			getLideres();
 		} else if(valor == 'nome'){
@@ -17,9 +17,9 @@
 			$('#div_conteudo').html("");
 		}
 	}
-	
+
 	function request(url, data, type) {
-		
+
 		return $.ajax({
 			url: url,
 			type: type,
@@ -31,7 +31,7 @@
 
 	function pesquisar() {
 		if($('#filtro').val() == '') return;
-		
+
 		var data = {
 			'type': $('#filtro').val(),
 			'content': $('#conteudo').val()
@@ -42,12 +42,12 @@
 				console.log('Erro  - ', response);
 			});
 	}
-	
+
 	function montarCelulas(response) {
-		
+
 		var html = "";
 		for(var i in response) {
-			var nome_lider = (!response[i].lider) ? "" : response[i].lider.nome;  
+			var nome_lider = (!response[i].lider) ? "" : response[i].lider.nome;
             html += "<tr>"+
 	            		"<td>"+response[i].nome+"</a></td>"+
 	            		"<td class='hidden-phone'>"+nome_lider+"</td>"+
@@ -67,22 +67,22 @@
 				console.log('Erro  - ', response);
 			});
 	}
-	
+
 	function montarComboLideres(response) {
 		var html = "<select name='conteudo' id='conteudo' class='form-control'>"+
 				   "	<option>-- Selecione --</option>";
-		
-		
+
+
 		for(var i in response) {
 			html += "	<option value='"+response[i].id+"'>"+response[i].nome+"</option>";
 		}
 		html += "</select>";
 		$('#div_conteudo').html(html);
 	}
-	
+
 	function mostrarFiltro() {
 		var html = "<input type='text' id='conteudo' name='conteudo' class='form-control'>";
 		$('#div_conteudo').html(html);
 	}
-	
+
 })();
